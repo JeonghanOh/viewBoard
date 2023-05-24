8c5ed5138a8101abb15da6bc17c44b48d7c55841
TWhy <lail_mind@naver.com>
2023-05-24 12:48:27 +0900

    tool

---
+++ src/main/java/viewboard/ViewboardApplication.java
@@ -0,0 +1,13 @@
+package viewboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/viewboard/ViewboardApplicationTests.java
@@ -0,0 +1,13 @@
+package viewboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
