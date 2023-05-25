bea702babc29e19610b125443a5bf4cd19c794e3
TWhy <lail_mind@naver.com>
2023-05-25 10:13:42 +0900

    jpa jsp

---
+++ src/main/java/viewboard/ViewboardpApplication.java
@@ -0,0 +1,13 @@
+package viewboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewboardpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewboardpApplication.class, args);
+	}
+
+}
